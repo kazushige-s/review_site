ccfa0c6b2e8de7451c1cdaf98df1477646b92b4b
Kazushige Shirahama <the.stone.monky.6317@gmail.com>
2023-05-28 16:49:56 +0900

    🐛

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 import { Button } from "@mantine/core";
 import Link from "next/link";
+import { Header } from "src/app/header";
 
 export default function Home() {
   return (
     <div>
+      <Header />
       <h1 className="text-3xl text-blue-500">Hello Next.js13 template</h1>
       <h2>h2です</h2>
       <h3>h3です</h3>
