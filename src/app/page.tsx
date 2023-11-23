50e56ac4c1db229a3022bb6390a33079ac164fb6
Kazushige Shirahama <the.stone.monky.6317@gmail.com>
2023-11-23 17:23:32 +0900

    📝 ドキュメントを更新

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Button } from "@mantine/core";
+import { Button, Space } from "@mantine/core";
 import Link from "next/link";
 import { Header } from "src/app/header";
 
@@ -7,12 +7,25 @@ export default function Home() {
   return (
     <div>
       <Header />
-      <h1 className="text-3xl text-blue-500">Hello Next.js13 template</h1>
-      <h2>h2です</h2>
-      <h3>h3です</h3>
-
+      <Space h="md" />
+      <h1 className="text-3xl text-blue-500">Reactの勉強方法</h1>
+      <h2>Udemyの講座</h2>
+      <ul>
+        <li>
+          最新ver対応済】モダンJavaScriptの基礎から始める挫折しないためのReact入門
+        </li>
+        <Link href="https://www.udemy.com/share/103Fxl3@gFCfFCgSIxZVEm53Ih-8GEU98yrt38hFGcDC5B-hqwnqVAiLz6kV4Km78MM3nSoJuA==/">
+          <Button variant="subtle">Udemyの講座へジャンプ</Button>
+        </Link>
+        <li>
+          Reactに入門した人のためのもっとReactが楽しくなるステップアップコース完全版
+        </li>
+        <Link href="https://www.udemy.com/share/104d6k3@15TCXGei2dYt6HQEgXao9JMdzAJ3ZIxY1iRBlFIiQB_TKVCP47u6-cuXYHCf1-1msw==/">
+          <Button variant="subtle">Udemyの講座へジャンプ</Button>
+        </Link>
+      </ul>
       <Link href="page1">
-        <Button>page1へ</Button>
+        <Button>Pythonの勉強方法へ</Button>
       </Link>
     </div>
   );
